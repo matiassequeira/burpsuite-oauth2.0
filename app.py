86fcd42fd246ca8b441bf2712b5f4bf9645a47af
James Lester <james.lester@Jamess-MBP.localdomain>
2022-01-11 12:11:13 -0800

    Added Changes to detectOAuth Function

--- app.py
+++ app.py
@@ -10,19 +10,9 @@ from burp import IScannerCheck
 from burp import IScanIssue
 from array import array
 from java.io import PrintWriter
-
-newResponseRequest = '''GET / HTTP/1.1
-Host: 
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0
-Accept: application/json, text/plain, */*
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate
-Referer: 
-Content-Type: application/json; charset=utf-8
-Connection: close
+from java.net import URL
 
 
-'''
 
 # Fields within request to Auth Provider
 # client_id
@@ -32,7 +22,9 @@ Connection: close
 # nonce
 # state
 
-OAuthParameters = ["client_id", "redirect_uri", "response_type", "scope", "state"]
+
+
+OAuthUrlsIdentified = []
 
 IMPLICIT_FLOW=0
 CODE_FLOW=1
@@ -69,29 +61,17 @@ class BurpExtender(IBurpExtender, IHttpListener, IProxyListener, IScannerListene
         print(self._helpers)
 
     def extensionUnloaded(self):
-        self._stdout.println("Extension was unloaded")
+        print "Extension was unloaded"
 
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
         # Check if Message is a Response, and see what tool it came from. This is just helping with print formatting and tracking.
-        #print("messageInfo: ", messageInfo)
+        #Check if Message is a Request and then check if it is coming from tool "Proxy".  If so, then analyze the request and go through thev parameters and see if any match known OAuth parameters. If so, Report.
         if messageIsRequest:
             if str(self._callbacks.getToolName(toolFlag)) == "Proxy":
                 #print("Proxy is receiving a Request")
                 analyzedRequest = self._helpers.analyzeRequest(messageInfo.getRequest())
-                #print ("Headers are: ", analyzedRequest.getHeaders())
-                if analyzedRequest.getParameters():
-                    analyzedParameters = analyzedRequest.getParameters()
-                    for parameter in analyzedParameters:
-                        #print parameter.getName().lower()
-                        for OAuthParameter in OAuthParameters:
-                            #print "OauthParameter", OAuthParameter
-                            #print "Parameter", parameter.getName()
-                            if parameter.getName().lower()  == OAuthParameter:
-                                print "FOUND AN OAUTH PARAMETER", OAuthParameter
-                        
-                
-            #else:
-                #print("Incomimg Tool Flag = " + str(self._callbacks.getToolName(toolFlag)) + " Message is Request")
+                detectOAuth(messageInfo, analyzedRequest)
+                    
         
         else:
             responseInfo = self._helpers.analyzeResponse(messageInfo.getResponse())
@@ -100,33 +80,47 @@ class BurpExtender(IBurpExtender, IHttpListener, IProxyListener, IScannerListene
             #print("responseReceived: ", responseReceived)
             if str(self._callbacks.getToolName(toolFlag)) == "Extender":
                 print("Extensions Response has been Received")
-            #else:
-                #print("Incomimg Tool Flag = " + str(self._callbacks.getToolName(toolFlag)) + " Message is Response")
                 
-        # Check if Request and Check if Tool is Repeater or Scanner (Those are the tools we care about at the moment)
-        # if not messageIsRequest and (str(self._callbacks.getToolName(toolFlag)) == "Repeater" or str(self._callbacks.getToolName(toolFlag)) == "Scanner"):
-        #     print("We have a Response from a Tool we care about, Lets see if it meets the condition for testing")
-        #     # Get our Responses some help
-        #     responseInfo = self._helpers.analyzeResponse(messageInfo.getResponse())
-        #     responseReceived = self._helpers.bytesToString(messageInfo.getResponse()).encode('utf-8')
-            
-            
-        #     # If response is 202 (Currenlty identified as being special circumstance for received response we want to look at)
-        #     if responseInfo.getStatusCode() == 202:
-        #         print("Response is 202, Envoking Extension Functionality.Generating a 2nd Request and Retieving its Response")
-        #         # Build new Request to make (Get our Getter. This is also where we use our stored response payload)
-        #         try:
-        #             newResponseRequestBytes = self._helpers.stringToBytes(newResponseRequest)
-        #             newResponse = self._callbacks.makeHttpRequest("westus.api.cognitive.microsoft.com", 443, True, newResponseRequestBytes)
-        #             # Set new Response to our old response (Shortcutting the 2nd Order)
-        #             messageInfo.setResponse(newResponse)
-        #             print("Response Received and Original Call has been Changed (Successful Extension Call)")
-        #         except:
-        #             # Something Happend With the Code, So we just moved on.
-        #             print("Exception Raised, Extension Functionaliity Cancelled")
-        #     else:
-        #         # Response did not meet condition, so we move on.
-        #         print("Non 202 Status Code Received " + str(responseInfo.getStatusCode()))
+def detectOAuth(messageInfo, analyzedRequest):
+    OAuthParameters = { "client_id": False,
+                        "redirect_uri": False,
+                        "response_type" : False,
+                        "scope": False,
+                        "state": False
+                    }
+    
+    OAuthIdentified = False
+    responseTypeIdentified = None
+    #print ("Headers are: ", analyzedRequest.getHeaders())
+    if analyzedRequest.getParameters():
+        analyzedParameters = analyzedRequest.getParameters()
+        for parameter in analyzedParameters:
+            #print parameter.getName().lower()
+            for OAuthParameter in OAuthParameters:
+                #print "OauthParameter", OAuthParameter
+                #print "Parameter", parameter.getName()
+                if parameter.getName().lower()  == OAuthParameter:
+                    #print "FOUND AN OAUTH PARAMETER", OAuthParameter
+                    OAuthParameters[OAuthParameter] = True
+                    if parameter.getName().lower() == "response_type":
+                        responseTypeIdentified = parameter.getValue().lower()
+                    
+        if OAuthParameters["client_id"] and OAuthParameters["response_type"]:
+            OAuthIdentified = True
+            messageService = messageInfo.getHttpService()
+            if str(messageService) not in str(OAuthUrlsIdentified):
+                OAuthUrlsIdentified.append(messageService)
+                print "------   New OAuth Identified   ------"
+                print "URL observed was : " + messageService.getProtocol() + "://" + messageService.getHost() + " using Port : " + str(messageService.getPort())
+                if responseTypeIdentified != None:
+                    print "-- Authorization Code Type Detected as : " + responseTypeIdentified + " --"
+                print " Parameters observed in the request indicating OAuth presence were :"
+                for item, value in OAuthParameters.items():
+                    if value == True:
+                        print "     > " + item
+                
+            else:
+                print "Existing OAuth Url Observed : " + messageService.getProtocol() + "://" + messageService.getHost()
 
 def get_oauth_flow_mode(request):
     return
